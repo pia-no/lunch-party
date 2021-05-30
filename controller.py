acf1dbac987dc0c355d95e15cd2ac8b2cbdb0dbe
pia.no <pia.no@kakaomobility.com>
2021-05-30 23:29:14 +0900

    first

---
+++ controller.py
@@ -0,0 +1,26 @@
+import json
+import test
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/absent", methods=['GET'])
+def absent():
+    test.absent("absent_test")
+    return json.dumps({})
+
+@app.route("/join", methods=['POST'])
+def join():
+    test.join("POST_JOIN")
+    return json.dumps({})
+
+
+@app.route("/join", methods=['GET'])
+def join2():
+    test.join("GET_JOIN")
+    return json.dumps({})
+
+@app.route("/party", methods=['GET'])
+def getParty():
+    return jsonify(test.getParty())
+app.run()
